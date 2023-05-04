ee966cab64b684cf4ffdc9413188db6ae6436aa2
Oleg Gavrilin <oleg@minterest.com>
2023-05-04 11:25:13 +0300

    style fixes

--- src/components/slider/Slider.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-export const Slider: React.FC = () => {
-  const [v, setV] = useState(0);
-  return (
-    <input type='range' value={v} onChange={(e) => setV(+e.target.value)} />
-  );
-};
